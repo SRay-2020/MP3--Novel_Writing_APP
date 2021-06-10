1b7fb7b99bd83ad343d58bff0b6984e7cd69a047
SRay-2020 <stephen.r.ray@gmail.com>
2021-06-10 16:46:17 +0000

    Fix run errors on app.py

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
 import os
-from flask import (
-    Flask, flash, render_template,
-    redirect, request, session, url_for)
-from flask_pymongo import PyMongo
-from bson.objectId import ObjectId
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask
+    # , flash, render_template,
+    # redirect, request, session, url_for)
+# from flask_pymongo import PyMongo
+# from bson.objectId import ObjectId
+# from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
 
+# app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+# app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+# app.secret_key = os.environ.get("SECRET_KEY")
 
-app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.secret_key = os.environ.get("SECRET_KEY")
-
-mongo = PyMongo(app)
+# mongo = PyMongo(app)
 
 @app.route("/")
 def hello():
@@ -23,5 +22,5 @@ def hello():
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-            port = int(os.environ.get("POST")),
+            port = int(os.environ.get("PORT")),
             debug = True)
