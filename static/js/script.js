f755191c7de18e6cf4787b473526905a27068bdf
SRay-2020 <stephen.r.ray@gmail.com>
2021-06-12 16:09:51 +0000

    Add visual book cards with flip

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,18 @@
-  $(document).ready(function(){
+$(document).ready(function(){
     $('.sidenav').sidenav();
-  });
+    $('.collapsible').collapsible();
+});
+
+// const card = document.querySelector('.card__inner');
+
+// card.addEventListener('click', function() {
+//     card.classList.toggle('is-flipped')
+// });
        
+
+const card = document.querySelectorAll(".card__inner");
+
+function flipCard() {
+  this.classList.toggle('is-flipped');
+}
+card.forEach((card) => card.addEventListener("click", flipCard));
